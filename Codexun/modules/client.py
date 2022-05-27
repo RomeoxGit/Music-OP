934b1faca83015941753d9c783c291e590c57fa4
RomeoxGit <98481727+RomeoxGit@users.noreply.github.com>
2022-05-27 14:28:17 +0530

    Update client.py

--- Codexun/modules/client.py
+++ Codexun/modules/client.py
@@ -606,7 +606,7 @@ If you didn't know how to make your own bot then contact us at @M8N_SUPPORT and 
 **Thanks !!**""",
        reply_markup=InlineKeyboardMarkup(
             [[
-                    InlineKeyboardButton("Get Repo 📦", url=f"https://github.com/PavanMagar/CodexunMusicBot")
+                    InlineKeyboardButton("Get Repo 📦", url=f"https://github.com/UnknownMortal/M8N-Music-Bot")
                 ],
               [InlineKeyboardButton("🔙  Back Home", callback_data="cbabout")]]
         ),
